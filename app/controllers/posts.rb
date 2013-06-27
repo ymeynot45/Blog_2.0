7b22ac48ff19447f6471e612679d89d72efba3c2
Apprentice <apprentice@dbc-chicago-009.local>
2013-06-27 16:56:50 -0500

    made tags visable on posts

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -3,4 +3,20 @@ get '/posts/create' do
 end
 
 post '/create' do
+  @post = Post.create(params[:blog])
+  post_id = @post.id
+  redirect "/posts/#{post_id}"
+end
+
+get '/posts' do
+  @all_posts = Post.all
+  erb :"posts/index"
+end
+
+# Unicorns is our join table with post_id and tag_id
+
+get '/posts/:id' do
+  @indiv_post = Post.find(params[:id])
+  @tags = @indiv_post.tags.pluck(:name)
+  erb :"posts/individual_post"
 end
